5273821602559fae9904d3ee48676b17c0f0be7c
yajatvishwak <yajat472001@gmail.com>
2021-07-07 17:35:11 +0530

    👌 IMPROVE:  hamburger

--- src/App.svelte
+++ src/App.svelte
@@ -9,17 +9,61 @@
   import Tailwind from "./components/Tailwind.svelte";
   import Headroom from "svelte-headroom";
   import Modal from "./components/Modal.svelte";
+  let openMenu = false;
 </script>
 
 <Tailwind />
 <Headroom>
   <div
-    class="m-4 mt-0 p-4  flex items-center justify-evenly max-w-screen-lg mr-auto ml-auto bg-white "
+    class="m-4 mt-0 p-4  md:flex items-center justify-evenly max-w-screen-lg mr-auto ml-auto bg-white "
   >
-    <div class="title font-black font-inter text-xl sm:text-2xl">Renteefy</div>
-
-    <nav class="flex md:hidden">expand</nav>
-
+    <div class="flex justify-between ">
+      <div class="title font-black font-inter text-xl sm:text-2xl">
+        Renteefy
+      </div>
+      <nav class="flex md:hidden">
+        {#if !openMenu}
+          <img
+            on:click={() => (openMenu = !openMenu)}
+            src="https://img.icons8.com/material-rounded/32/000000/menu--v1.png"
+          />
+        {:else}
+          <img
+            on:click={() => (openMenu = !openMenu)}
+            src="https://img.icons8.com/windows/32/000000/macos-close.png"
+          />
+        {/if}
+      </nav>
+    </div>
+    {#if openMenu}
+      <nav class="flex flex-col">
+        <a
+          href="#whatisrenteefy"
+          class="navigation hover:bg-pink-600 hover:text-white mx-4 rounded-lg p-2"
+          >About</a
+        >
+        <a
+          href="#features"
+          class="navigation hover:bg-pink-600 hover:text-white mx-4 rounded-lg p-2"
+          >Features</a
+        >
+        <a
+          href="#faq"
+          class="navigation hover:bg-pink-600 hover:text-white mx-4 rounded-lg p-2"
+          >FAQs</a
+        >
+        <a
+          href="#contactus"
+          class="navigation hover:bg-pink-600 hover:text-white mx-4 rounded-lg p-2"
+          >Contact us</a
+        >
+        <a
+          href="#newsletter"
+          class="navigation hover:bg-pink-600 hover:text-white mx-4 rounded-lg p-2"
+          >Newsletter</a
+        >
+      </nav>
+    {/if}
     <nav class="hidden md:flex">
       <a
         href="#whatisrenteefy"
